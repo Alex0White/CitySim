947d9dcc5abcd6f6616d6433bd31bd17d447200d
Alex <alexjwhitex@gmail.com>
2017-06-06 00:54:10 +1200

    got some of it working having issue with using seed to create a new seed

--- CityLocations.py
+++ CityLocations.py
@@ -2,33 +2,91 @@
 import random
 
 
+
+
+class RNG(object):
+    def __init__(self, seed=58):
+        self.seed = seed
+
+    def get_set_seed(self):
+        self.seed = self.new_seed(self.seed)
+        return self.seed
+
+    def get_seed(self):
+        return self.seed
+
+    def set_seed(self, a_seed):
+        self.seed = a_seed
+
+
+
+    def new_seed(self, the_seed):
+        random.seed(the_seed)
+        num = random.random()
+        new_seed = int(num * 100)
+
+        return new_seed
+
+
+
+
 class Car(object):
     """this is a Car"""
     def __init__(self, driver_number):
         self.driver_number = driver_number
-        self.location = random.randint(1, 4)
+        self.location = random.randint(0, 3)
+        self.next_location = 0
+
+
+
+
+
     def set_driver_number(self, num):
         """sets the driver number 1 through to 5 according to the requirements"""
         self.driver_number = num
 
+    def get_driver_number(self):
+        return self.driver_number
+
+    def set_location(self, num):
+        """sets location should be a number 0 to 3"""
+        self.location = num
+
+    def get_location(self):
+        return self.location
+
+    def set_next_location(self, num):
+        """sets location should be a number 0 to 3"""
+        self.next_location = num
+
+    def get_next_location(self):
+        return self.next_location
+
+
+
     def choose_direction(self, the_seed):  # seed must be a int
         """psudo randomly chooses between 1 and 2"""
-        random.seed(the_seed)
-        num = random.random()
-        if num > 0.5:
+
+        if the_seed > 50:
             num = 1
         else:
             num = 0
         return num
 
+    def road_picker(self, roads, location, direction):
+        two_roads = roads[location]
+        the_road = two_roads[direction]
+        return the_road
+
+
     def choose_to_leave_city(self, the_seed):
-        random.seed(the_seed)
-        num = random.random()
-        if num > 0.8:
-            num = 1
+
+
+        if the_seed > 49:
+            return False
         else:
-            num = 0
-        return num
+            return True
+
 
     def new_seed(self, the_seed):
         random.seed(the_seed)
@@ -42,29 +100,27 @@ class Car(object):
         if direction == 1 and index == (len(array) - 1):
             new_index = 0
         elif direction == 0 and index == 0:
-            new_index = len(array - 1)
+            new_index = len(array) - 1
         elif direction == 1:
             new_index = (index + 1)
         else:
             new_index = index - 1
-        return array[index], new_index, array[new_index]
+        return new_index
 
-    def driver_heading(self, location, direction, roads, driver_number ):
+    def driver_heading(self, driver_number, location, next_location, roads):
         """Driver 3 heading from Mayfair to Stortford lodge via [option street 3]."""
 
-        driver_number
-        roads[location]
-        next_location = location
-        return "Driver " + str(driver_number) + " heading from "+ location +" to "+ \
-               next_location +" lodge via "+ roads +"."
+
+
+        return "Driver " + str(driver_number) + " heading from "+ location +" to "+ next_location +" via "+ roads +"."
 
 
 
     def exit_city(self, location=None):
         """doc"""
-        if location == "Karamu Rd":
+        if location == 1 or 2:
             message = "the driver has gone to Napier"
-        elif location == "Omahu Rd":
+        elif location == 2 or 3:
             message = "the driver has gone to Flaxmere."
         else:
             message = None
@@ -87,22 +143,62 @@ class Car(object):
             return None
         return " \n -----"
 
+def main():
+    seed = RNG(int(input("Enter an integer: ")))
+    for i in range(1, 6):
+        c = Car(i)
+        four_locations = ["Akina", "Mayfair", "Mahora", "Storford Lodge"]
+        roads = [["Murdoch Rd", "Riverslea Rd" ], ["Windsor Ave", "Grove Rd"], ["Fredrick St", "Pakowhai Rd"], ["Maraekakaho Rd", "Wall Rd => Southland Rd => Murdoch Rd"]]
+
+        stay = True
+        while stay:
+            direction = c.choose_direction(seed.get_seed())
+            c.set_next_location(c.circular_list(four_locations, c.get_location(), direction ))
+            road = c.road_picker(roads, c.get_location(), direction)
+
+            first = four_locations[c.get_location()]
+            print(first)
+
+            second = four_locations[c.get_next_location()]
+            print(second)
+            print(c.driver_heading(c.get_driver_number(), first, second, road))
+
+            seed.set_seed(seed.new_seed(seed.get_seed()))
+            stay = c.choose_to_leave_city(seed.get_seed())
+            seed.set_seed(seed.new_seed(seed.get_seed()))
+
+
+            c.set_location(c.get_next_location())
+
+        print("exit city " + str(c.get_driver_number()))
+
+if __name__ == '__main__':
+    main()
+
 
 ##test stuff
-seed = int(input("Enter an integer: "))
-c = Car(1)
 
-n = []
+##seed = int(input("Enter an integer: "))
+##c = Car(1)
 
-while(exit == 0):
+##n = []
 
-    n.append(c.choose_direction(seed))
-    seed = c.new_seed(seed)
-print(n)
+# while(exit == 0):
+#
+#     n.append(c.choose_direction(seed))
+#     seed = c.new_seed(seed)
+# print(n)
+#
+#
+# four_locations = ["Akina", "Mayfair", "Mahora", "Storford Lodge"]
 
 
-four_locations = ["Akina", "Mayfair", "Mahora", "Storford Lodge"]
-print(c.circular_list(four_locations, 3, 1))
+##print(c.circular_list(four_locations, 3, 1))
+
+##print(c.circular_list(four_locations, 0, 0))
+##print(c.circular_list(four_locations, 1, 0))
+##print(c.circular_list(four_locations, 4, 1))
+
 
 exits = [ "Heratanga St / Havelock Rd", "Karamu Rd", "Omahu Rd", "Railway Rd"] 
             #Right          #left
