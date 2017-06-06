8ef4d7a4c46413144d5748f204dd859143a2a5b6
Alex <alexjwhitex@gmail.com>
2017-06-07 00:01:59 +1200

    finished main program but need to finish testing

--- CityLocations.py
+++ CityLocations.py
@@ -1,16 +1,9 @@
 """doc"""
 import random
 
-
-
-
 class RNG(object):
     def __init__(self, seed=58):
         self.seed = seed
-
-    def get_set_seed(self):
-        self.seed = self.new_seed(self.seed)
-        return self.seed
 
     def get_seed(self):
         return self.seed
@@ -19,6 +12,7 @@ class RNG(object):
         self.seed = a_seed
 
     def new_seed(self, the_seed):
+        """create a new seed using a seed"""
         random.seed(the_seed + 1)
         num = random.random()
         new_seed = int(num * 100)
@@ -31,8 +25,6 @@ class Car(object):
         self.location = 1
         self.next_location = 0
         self.meet_with_john_num = 0
-
-
 
     def meet_with_john_num_plusone(self):
         self.meet_with_john_num += 1
@@ -60,7 +52,6 @@ class Car(object):
 
     def choose_direction(self, the_seed):  # seed must be a int
         """psudo randomly chooses between 1 and 2"""
-
         if the_seed > 50:
             num = 1
         else:
@@ -68,13 +59,14 @@ class Car(object):
         return num
 
     def road_picker(self, roads, location, direction):
+
         two_roads = roads[location]
         the_road = two_roads[direction]
         return the_road
 
     def choose_to_leave_city(self, the_seed):
 
-        if the_seed > 49:
+        if the_seed > 70:
             return False
         else:
             return True
@@ -111,10 +103,12 @@ class Car(object):
 
     def driver_heading(self, driver_number, location, next_location, roads):
         """Driver 3 heading from Mayfair to Stortford lodge via [option street 3]."""
-        return "Driver " + str(driver_number) + " heading from "+ location +" to "+ next_location +" via "+ roads +"."
+        return "Driver " + str(driver_number) + " heading from "+ location \
+               +" to "+ next_location +" via "+ roads +"."
 
     def exit_city_street(self, location, street):
-        return "Driver " + str(self.driver_number) + " heading from " + location + " to Outside City via " + street + "."
+        return "Driver " + str(self.driver_number) + " heading from " +\
+               location + " to Outside City via " + street + "."
 
     def exit_city(self, location=None):
         """doc"""
@@ -123,7 +117,7 @@ class Car(object):
         elif location == "Omahu Rd":
             message = message = "Driver " + str(self.driver_number) + " has gone to Flaxmere"
         else:
-            message = ''
+            message = None
         return message
 
     def met_john_message(self):
@@ -151,40 +145,54 @@ class Car(object):
         return "-----"
 
 def main():
-    seed = RNG(int(input("Enter an integer: ")))
-    for i in range(1, 6):
-        c = Car(i)
-        four_locations = ["Akina", "Mayfair", "Mahora", "Storford Lodge"]
-        roads = [["Murdoch Rd", "Riverslea Rd" ], ["Windsor Ave", "Grove Rd"], ["Fredrick St", "Pakowhai Rd"], ["Maraekakaho Rd", "Wall Rd => Southland Rd => Murdoch Rd"]]
-        exits = ["Heratanga St / Havelock Rd", "Karamu Rd", "Omahu Rd", "Railway Rd"]
-        stay = True
-        while stay:
-            direction = c.choose_direction(seed.get_seed())
-            c.set_next_location(c.circular_list(four_locations, c.get_location(), direction ))
-            road = c.road_picker(roads, c.get_location(), direction)
+    with open('file.txt', 'a') as f:
+        while True:
+            try:
+                the_input = (int(input("Enter an integer: ")))
+                break
+            except ValueError:
+                print("Oops! that was not a interger try again")
+        seed = RNG(the_input)
 
-            first = four_locations[c.get_location()]
-            #print(first)
-
-            second = four_locations[c.get_next_location()]
-            #print(second)
-            print(c.driver_heading(c.get_driver_number(), first, second, road))
-            if c.location == 0:
-                c.meet_with_john_num_plusone()
-            seed.set_seed(seed.new_seed(seed.get_seed()))
-            stay = c.choose_to_leave_city(seed.get_seed())
-            ##seed.set_seed(seed.new_seed(seed.get_seed()))
+        for i in range(1, 6):
+            c = Car(i)
+            four_locations = ["Akina", "Mayfair", "Mahora", "Storford Lodge"]
+            roads = [["Murdoch Rd", "Riverslea Rd" ], ["Windsor Ave", "Grove Rd"], \
+                     ["Fredrick St", "Pakowhai Rd"], ["Maraekakaho Rd", "Wall Rd => Southland Rd => Murdoch Rd"]]
+            exits = ["Heratanga St / Havelock Rd", "Karamu Rd", "Omahu Rd", "Railway Rd"]
+            stay = True
 
 
-            c.set_location(c.get_next_location())
-        exit_direction = c.choose_direction(seed.get_seed())
-        exit_street = exits[c.exit_circular_list(exits, c.get_location(), exit_direction)]
-        current_location = four_locations[c.get_location()]
-        print(c.exit_city_street(current_location, exit_street))
-        if c.exit_city(exit_street) != None:
-            print(c.exit_city(exit_street))
-        print(c.met_john_message())
-        print(c.car_final_messages())
+
+            while stay:
+                direction = c.choose_direction(seed.get_seed())
+                c.set_next_location(c.circular_list(four_locations, c.get_location(), direction ))
+                road = c.road_picker(roads, c.get_location(), direction)
+
+                first = four_locations[c.get_location()]
+                #print(first)
+
+                second = four_locations[c.get_next_location()]
+                #print(second)
+                print(c.driver_heading(c.get_driver_number(), first, second, road), file=f)
+
+                seed.set_seed(seed.new_seed(seed.get_seed()))
+                stay = c.choose_to_leave_city(seed.get_seed())
+                ##seed.set_seed(seed.new_seed(seed.get_seed()))
+
+
+                c.set_location(c.get_next_location())
+                if c.location == 0:
+                    c.meet_with_john_num_plusone()
+
+            exit_direction = c.choose_direction(seed.get_seed())
+            exit_street = exits[c.exit_circular_list(exits, c.get_location(), exit_direction)]
+            current_location = four_locations[c.get_location()]
+            print(c.exit_city_street(current_location, exit_street), file=f)
+            if c.exit_city(exit_street) != None:
+                print(c.exit_city(exit_street), file=f)
+            print(c.met_john_message(), file=f)
+            print(c.car_final_messages(), file=f)
 
 if __name__ == '__main__':
     main()
--- CityLocationsTest.py
+++ CityLocationsTest.py
@@ -2,6 +2,10 @@ import unittest
 from CityLocations import Car
 
 class CarTest(unittest.TestCase):
+    def test_meet_with_john_num_plusone(self):
+        c = Car(1)
+        self.assertEqual(c.meet_with_john_num, )
+
     def test_exit_city_Karamu_Road(self):
         c = Car(1)
         self.assertEqual(c.exit_city("Karamu Rd"), "the driver has gone to Napier")
@@ -13,4 +17,8 @@ class CarTest(unittest.TestCase):
     def test_exit_city_None(self):
         c = Car(1)
         self.assertEqual(c.exit_city(),None)
+
+
+
+
         
